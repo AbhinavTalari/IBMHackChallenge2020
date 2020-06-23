28fa13fd9a80a616acb2ba2ac6e8a6d718b3901c
Abhinav Talari <49162896+AbhinavTalari@users.noreply.github.com>
2020-06-23 18:45:51 +0530

    Some Changes

--- backend/chatbot/luna/forms.py
+++ backend/chatbot/luna/forms.py
@@ -5,8 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField(required=True) 
-    resume=forms.FileField()
-    
     class Meta:
          model=User
          fields=['username','email','password1','password2']
--- backend/chatbot/luna/urls.py
+++ backend/chatbot/luna/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.render_home, name='luna-home'),
     path('botpage/',views.render_botpage,name='luna-bot-welcome'),
-    path('botpage/login/',views.render_login,name='luna-bot-login'),
-    path('botpage/signup/',views.render_signup,name='luna-bot-signup')
+    path('botpage/login/',auth_views.LoginView.as_view(template_name='luna/login.html'),name='luna-bot-login'),
+    path('botpage/signup/',views.render_signup,name='luna-bot-signup'),
+    path('botpage/logout/',auth_views.LogoutView.as_view(template_name='luna/logout.html'),name='luna-bot-logout'),
    
 ]
